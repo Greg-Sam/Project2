a00f01dba6141a90e2fe668d27641f6de0ed7a0c
Greg Samuelson <grssam@gmail.com>
2020-12-07 12:34:32 -0800

    models ready

---
+++ models/Post.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Post extends Model { }
+
+Post.init({
+  // name: {
+  //   type: DataTypes.STRING,
+  //   allowNull: false
+  // },
+  // email: {
+  //   type: DataTypes.STRING,
+  //   allowNull: false
+  // },
+  // phone: {
+  //   type: DataTypes.INTEGER,
+  //   allowNull: false
+  // }
+}, { sequelize, modelName: 'post' })
+
+module.exports = Post
---
+++ routes/postRouts.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const { Post } = require('../models')
+
+router.get('/posts', (req, res) => {
+  Post.findAll()
+    .then(posts => res.json(posts))
+    .catch(err => console.log(err))
+})
+
+router.post('/posts', (req, res) => {
+  Post.create(req.body)
+    .then(post => res.json(post))
+    .catch(err => console.log(err))
+})
+
+router.put('/posts/:id', (req, res) => {
+  Post.update(req.body, { where: { id: req.params.id } })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
+})
+
+router.delete('/posts/:id', (req, res) => {
+  Post.destroy({ where: { id: req.params.id } })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
