45b5bd338008ba974580821a4c4898aa162400ac
Greg Samuelson <grssam@gmail.com>
2020-12-07 11:59:34 -0800

    created routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const { User } = require('../models')
+
+router.get('/users', (req, res) => {
+  User.findAll()
+    .then(users => res.json(users))
+    .catch(err => console.log(err))
+})
+
+router.post('/users', (req, res) => {
+  User.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => console.log(err))
+})
+
+router.put('/users/:id', (req, res) => {
+  User.update(req.body, { where: { id: req.params.id } })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
+})
+
+router.delete('/users/:id', (req, res) => {
+  User.destroy({ where: { id: req.params.id } })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
