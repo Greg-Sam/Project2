405590578c4b415c8f5d9c51bd4a575c49626627
Amee Parmar <amee05@yahoo.com>
2020-12-07 13:20:57 -0800

    creating db folder

--- models/Post.js
+++ models/Post.js
@@ -4,18 +4,19 @@ const sequelize = require('../db')
 class Post extends Model { }
 
 Post.init({
-  // name: {
-  //   type: DataTypes.STRING,
-  //   allowNull: false
-  // },
-  // email: {
-  //   type: DataTypes.STRING,
-  //   allowNull: false
-  // },
-  // phone: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false
-  // }
+  
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
 }, { sequelize, modelName: 'post' })
 
 module.exports = Post
--- models/User.js
+++ models/User.js
@@ -4,18 +4,23 @@ const sequelize = require('../db')
 class User extends Model { }
 
 User.init({
-  // name: {
-  //   type: DataTypes.STRING,
-  //   allowNull: false
-  // },
-  // email: {
-  //   type: DataTypes.STRING,
-  //   allowNull: false
-  // },
-  // phone: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false
-  // }
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+    
+  }
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
 }, { sequelize, modelName: 'user' })
 
 module.exports = User
