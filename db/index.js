7987b6a09b9916b59c0f0db421335176013d14e1
ky-lore <kclore3131@gmail.com>
2020-12-08 15:18:48 -0800

    working logic for axios post into users db table

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('pets_db', 'root', '1234', {
+const sequelize = new Sequelize('codebook_db', 'root', '1234', {
   host: 'localhost',
   dialect: 'mysql',
   define: {
--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,8 +1,33 @@
 document.getElementById('loginBtn').addEventListener('click', e => {
   e.preventDefault()
+  addUser()
+})
+
+const addUser = () => {
   let inputEmail = document.getElementById('inputEmail').value
+  let inputUsername = document.getElementById('inputUsername').value
+  let inputFirstName = document.getElementById('inputFirstName').value
+  let inputLastName = document.getElementById('inputLastName').value
   let inputPassword = document.getElementById('inputPassword').value
   let confirmedPassword = document.getElementById('inputConfirmPassword').value
-  let inputUsername = document.getElementById('inputUsername').value
-  console.log(inputUsername, inputEmail, inputPassword, confirmedPassword)
-})
+  console.log(inputFirstName, inputLastName, inputUsername, inputEmail, inputPassword, confirmedPassword)
+
+  if (checkPasswordMatch()) {
+    axios.post('/api/users', {
+      first_name: inputFirstName,
+      last_name: inputLastName,
+      email: inputEmail,
+      user_name: inputUsername,
+      password: inputPassword
+    })
+    .then(({ data }) => {
+        console.log(data)
+    })
+    .catch(err => console.error(err))
+  }
+}
+
+const checkPasswordMatch = (inputPassword, confirmedPassword) => {
+  let isMatch = inputPassword === confirmedPassword ? true : false
+  return isMatch
+}
