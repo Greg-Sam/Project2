a3df4afd1c9aaa306d9b5b538085602d6d2f2ab0
ky-lore <kclore3131@gmail.com>
2020-12-11 11:49:55 -0800

    bugfix

--- sql/seed.sql
+++ sql/seed.sql
@@ -5,13 +5,6 @@ VALUES ('Elisabeth', 'Windsor', 'ewindsor@email.com', 'Queen', 'password');
 INSERT INTO users (first_name, last_name, email, user_name, password) 
 VALUES ('Luke', 'Skywalker', 'lukee@email.com', 'Light Side', 'password');
 INSERT INTO users (first_name, last_name, email, user_name, password) 
-VALUES ('Harry', 'Potter', 'harry@email.com', 'ChosenOne', 'password');INSERT INTO users (first_name, last_name, email, user_name, password, pfp) 
-VALUES ('Darth', 'Vader', 'darth@email.com', 'DarkSide', 'password', 'https://upload.wikimedia.org/wikipedia/en/7/76/Darth_Vader.jpg');
-INSERT INTO users (first_name, last_name, email, user_name, password) 
-VALUES ('Elisabeth', 'Windsor', 'ewindsor@email.com', 'Queen', 'password');
-INSERT INTO users (first_name, last_name, email, user_name, password) 
-VALUES ('Luke', 'Skywalker', 'lukee@email.com', 'Light Side', 'password');
-INSERT INTO users (first_name, last_name, email, user_name, password) 
 VALUES ('Harry', 'Potter', 'harry@email.com', 'ChosenOne', 'password');
 
 INSERT INTO posts (post, user_id) VALUES ('I am your father, Luke!', 1);
